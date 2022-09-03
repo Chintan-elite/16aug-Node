c0ba8d46d448f146f5725cd752543165a7d59909
Chintan <chintan.tops@gmail.com>
2022-09-03 11:17:23 +0530

    git modules

---
+++ NodeLearning/os.js
@@ -0,0 +1,10 @@
+const os = require("os");
+
+
+console.log(os.arch())
+console.log(os.platform());
+console.log(os.version());
+console.log(os.type());
+console.log(os.totalmem() / 1024 / 1024 / 1024);
+console.log(os.freemem() / 1024 / 1024 / 1024);
+
---
+++ NodeLearning/pathmodule.js
@@ -0,0 +1,10 @@
+const path = require("path")
+
+
+const dirPath = __dirname;
+const filePath = __filename;
+const myPath = path.join(__dirname, "text.txt")
+
+console.log(dirPath);
+console.log(filePath);
+console.log(myPath);
