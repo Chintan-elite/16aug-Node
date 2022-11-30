5feb0a58b34de58290542bd605ea882d08923896
Chintan <chintan.tops@gmail.com>
2022-11-30 11:05:58 +0530

    project

---
+++ E-shop/middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken")
+const Admin = require("../model/Admin")
+
+const auth = async (req, resp, next) => {
+
+    const mytoken = req.cookies.jwt
+    try {
+        const ad = await jwt.verify(mytoken, process.env.S_KEY);
+
+        const adminData = await Admin.findOne({ _id: ad._id })
+
+        req.admin = adminData
+        req.token = mytoken
+        next()
+
+    } catch (error) {
+        resp.render("admin_login", {
+            msg: "please login first"
+        })
+    }
+
+}
+
+module.exports = auth
--- E-shop/router/adminRouter.js
+++ E-shop/router/adminRouter.js
@@ -1,16 +1,56 @@
 const express = require("express")
 const router = express.Router()
-
+const Admin = require("../model/Admin")
+const jwt = require("jsonwebtoken")
+const auth = require("../middleware/auth")
 
 router.get("/admin", (req, resp) => {
     resp.render("admin_login")
 })
 
-router.get("/adminhome", (req, resp) => {
+router.get("/adminhome", auth, async (req, resp) => {
     resp.render("admin_home")
 })
 
-router.post("/adminlogin", (req, resp) => {
-    resp.render("admin_home")
+router.post("/adminlogin", async (req, resp) => {
+
+    try {
+        const username = req.body.username;
+        const password = req.body.password;
+        // console.log(username + " " + password);
+
+        const admin = await Admin.findOne({ username })
+        //console.log(admin);
+        //console.log(admin.password);
+
+        if (admin.password === password) {
+
+            const token = await jwt.sign({ _id: admin._id }, process.env.S_KEY)
+            resp.cookie("jwt", token)
+            resp.render("admin_home", { data: admin })
+        }
+        else {
+            resp.render("admin_login", { msg: "invalid credentials" })
+        }
+
+
+    } catch (error) {
+        resp.render("admin_login", { msg: "invalid credentials" })
+    }
+
 })
+
+router.get("/adminlogout", auth, (req, resp) => {
+    const token = req.token
+    resp.clearCookie('jwt')
+    resp.render("admin_login")
+})
+
+
+router.get("/category", auth, (req, resp) => {
+    resp.render("admin_category")
+})
+
+
+
 module.exports = router
